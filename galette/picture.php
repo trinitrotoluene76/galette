4211a7dcf8e92a6c9435370802e225edbf2aeb25
trinitrotoluene76 <trinitrotoluene76@gmail.com>
2017-01-28 12:44:31 +0100

    Modifications de picture.php
    Correction du bug #34 du plugin subscription

--- galette/picture.php
+++ galette/picture.php
@@ -75,6 +75,10 @@ if (  isset($_GET['logo']) && $_GET['logo'] == 'true' ) {
             }
         }
 
+//Correction du bug #34 du plugin subscription
+$is_manager = true;
+//fin de la correction
+                    
         $picture = null;
         if ( $login->isAdmin()
             || $login->isStaff()
